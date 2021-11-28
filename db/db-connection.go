9e11fc4be8206e8c7fe9b7ea776a2d667c5ff5fc
ycak1r <ycaki[C[C[C[C[1;usufckr6194g@gmail.com>
2021-11-28 22:34:52 +0300

    add db directory

---
+++ db/db-connection.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+
+// DB name:	 		golang
+// DB username: 	root
+// DB password: 	root22YCA:
+// DB port: 		localhost:3306
+
+var urlDSN = "root:root22YCA:@tcp(localhost:3306)/golang?parseTime=true"
+var err error
+
+//database migrate.
+func DataMigration() {
+	Database, err = gorm.Open(mysql.Open(urlDSN), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Connection failed")
+	}
+	//Tables
+	Database.AutoMigrate(&Employee{})
+	Database.AutoMigrate(&Product{})
+	Database.AutoMigrate(&Users{})
+	Database.AutoMigrate(&Authorization{})
+}
